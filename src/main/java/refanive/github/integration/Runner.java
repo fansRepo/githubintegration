8bbc131224d16a6c3e3ff6897a6fdc245f72c7ac
bzyi <yfan61@sina.com>
2018-12-27 14:01:05 -0800

    My First Commit

---
+++ src/main/java/refanive/github/integration/Runner.java
@@ -0,0 +1,15 @@
+package refanive.github.integration;
+
+import javax.swing.*;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Welcome on GitHub",
+                "Information",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
+
